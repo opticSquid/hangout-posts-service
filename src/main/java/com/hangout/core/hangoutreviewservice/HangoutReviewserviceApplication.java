0b04143f84ea2ca3e366d694ae30683adecc2b53
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2023-11-23 11:30:59 +0530

    Added all primary dependencies

---
+++ src/main/java/com/hangout/core/hangoutreviewservice/HangoutReviewserviceApplication.java
@@ -0,0 +1,13 @@
+package com.hangout.core.hangoutreviewservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangoutReviewserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangoutReviewserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hangout/core/hangoutreviewservice/HangoutReviewserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hangout.core.hangoutreviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangoutReviewserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
