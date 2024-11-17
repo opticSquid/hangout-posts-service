1a483929e69c93618fbb6b85705d11f93b23b1a1
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2024-11-17 15:17:20 +0530

    changing root package name

---
+++ src/test/java/com/hangout/core/post/HangoutPostsServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.hangout.core.post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HangoutPostsServiceApplicationTests {
+    @Test
+    void contextLoads() {
+
+    }
+}
---
+++ src/test/java/com/hangout/core/post/services/PostServiceTests.java
@@ -0,0 +1,12 @@
+package com.hangout.core.post.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PostServiceTests {
+    @Test
+    void contextLoads() {
+
+    }
+}
